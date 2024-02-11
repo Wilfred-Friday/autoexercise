a7ff88506086ee930a55e7c70d83b56d90a7f8ed
Wilfred-Friday <wilfredo.tobar10@gmail.com>
2024-02-10 21:35:11 -0600

    Adding new elements

--- src/test/java/pageObject/Home_Page.java
+++ src/test/java/pageObject/Home_Page.java
@@ -1,4 +1,27 @@
 package pageObject;
 
+import org.openqa.selenium.By;
+
 public class Home_Page {
+
+    public By pHome_Logo(){
+        return By.xpath("//img[@alt='Website for automation practice']");
+    }
+
+    public By pLogin_Button(){
+        return By.xpath("//div[@class='shop-menu pull-right']/ul/li/a[contains(text(),' Signup / Login')]");
+    }
+
+    public By pUser_Logged(){
+        return By.xpath("//a[text()= ' Logged in as ']");
+    }
+
+    public By pDeleted_Button(){
+        return By.xpath("//a[text()= ' Delete Account']");
+    }
+
+    public By pDeleted_Account_Banner(){
+        return By.xpath("//h2[@data-qa= 'account-deleted']");
+    }
+
 }
--- src/test/java/pageObject/Signup_Login_Page.java
+++ src/test/java/pageObject/Signup_Login_Page.java
@@ -1,4 +1,108 @@
 package pageObject;
 
+import org.openqa.selenium.By;
+
+import java.util.Random;
+
 public class Signup_Login_Page {
+
+
+    public By pSingUp(){
+        return By.xpath("//div[@class='signup-form']/h2");
+    }
+
+    public By pSignUp_Button(){
+        return By.xpath("//button[@data-qa='signup-button']");
+    }
+
+    public By pForm_Banner(){
+        return By.xpath("//div[@class='login-form']/h2/b");
+    }
+
+    public By pRandom_Radio_Button(){
+        By pOption1 = By.xpath("//input[@id='id_gender1']");
+        By pOption2 = By.xpath("//input[@id='id_gender2']");
+        Random random = new Random();
+        // Generate a random number between 1 and 2 (inclusive)
+        int randomNumber = random.nextInt(2) + 1;
+        if(randomNumber == 1){
+            return pOption1;
+        }else{
+            return pOption2;
+        }
+    }
+
+    public By pName(){
+        return By.xpath("//input[@id='name']");
+    }
+
+    public By pEmail(){
+        return By.xpath("//input[@id='email']");
+    }
+
+    public By pPassword(){
+        return By.xpath("//input[@id='password']");
+    }
+
+    public By pCalendar_Days(){
+        return By.xpath("//select[@id='days']");
+    }
+
+    public By pCalendar_Months(){
+        return By.xpath("//select[@id='months']");
+    }
+
+    public By pCalendar_Years(){
+        return By.xpath("//select[@id='years']");
+    }
+
+    public By pNews_Letters_Option(){
+        return By.xpath("//input[@id='newsletter']");
+    }
+
+    public By pEnroll_News(){
+        return By.xpath("//input[@id='optin']");
+    }
+    public By pFirst_Name(){
+        return By.xpath("//input[@id='first_name']");
+    }
+    public By pLast_Name(){
+        return By.xpath("//input[@id='last_name']");
+    }
+    public By pCompany(){
+        return By.xpath("//input[@id='company']");
+    }
+    public By pAddress1(){
+        return By.xpath("//input[@id='address1']");
+    }
+    public By pAddress2(){
+        return By.xpath("//input[@id='address2']");
+    }
+    public By pCountry(){
+        return By.xpath("//select[@id='country']");
+    }
+    public By pState(){
+        return By.xpath("//input[@id='state']");
+    }
+    public By pCity(){
+        return By.xpath("//input[@id='city']");
+    }
+    public By pZipCode(){
+        return By.xpath("//input[@id='zipcode']");
+    }
+    public By pMobile_Number(){
+        return By.xpath("//input[@id='mobile_number']");
+    }
+    public By pCreate_Account_Button(){
+        return By.xpath("//button[@data-qa='create-account']");
+    }
+
+    public By pAccount_Created_Banner(){
+        return By.xpath("//h2[@data-qa='account-created']");
+    }
+
+    public By pContinue_Button(){
+        return By.xpath("//a[@data-qa='continue-button']");
+    }
+
 }
